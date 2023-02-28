a0a7966cbc709f6c93b9600220d0ca8b73b92df2
Fernando Nicolás <fersanti2896@gmail.com>
2023-02-28 13:12:09 -0600

    Autenticacion con Google con Firebase en providers.js

---
+++ src/firebase/providers.js
@@ -0,0 +1,31 @@
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { FirebaseAuth } from './config';
+
+const googleProvider = new GoogleAuthProvider();
+
+export const singWithGoogle = async() => {
+    try {
+        const result = await signInWithPopup( FirebaseAuth, googleProvider );
+
+        /* Se obtiene las credenciales */
+        // const credentials = GoogleAuthProvider.credentialFromResult( result );
+
+        const { displayName, email, photoURL, uid } = result.user;
+        
+        return {
+            ok: true,
+            displayName,
+            email,
+            photoURL,
+            uid
+        }   
+    } catch (error) {
+        const errorCode    = error.code;
+        const errorMessage = error.message;
+        
+        return {
+            ok: false,
+            errorMessage
+        }
+    }
+}
--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,3 +1,4 @@
+import { singWithGoogle } from "../../firebase/providers";
 import { checkingCredentials } from "./"
 
 export const checkingAuthentication = ( email, password ) => {
@@ -9,5 +10,9 @@ export const checkingAuthentication = ( email, password ) => {
 export const startGoogleSingIn = () => {
     return async( dispatch ) => {
         dispatch( checkingCredentials() );
+
+        /* Se llama la función de Firebase para la autenticación con Google */
+        const result = await singWithGoogle()
+        console.log(result)
     }
 }
